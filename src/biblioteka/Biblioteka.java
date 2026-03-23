f0141bcae0b4ae82714e2ed7ec536391cacfa939
Mihajlo Ristanovic <mr20220302@student.fon.bg.ac.rs>
2026-03-23 13:41:00 +0100

    Dodat opis metode pronadjiKnjigu.

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -38,6 +38,8 @@ public class Biblioteka implements BibliotekaInterface {
 	public List<Knjiga> vratiSveKnjige() {
 		return knjige;
 	}
+	
+	//Ova metoda za sada pretrazuje samo po naslovu
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
