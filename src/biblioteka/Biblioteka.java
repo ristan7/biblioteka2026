309b7474fd06cad5bf9574da588066e70a5c8b2c
Mihajlo Ristanovic <mr20220302@student.fon.bg.ac.rs>
2026-03-23 12:48:29 +0100

    Uvedena klasa Biblioteka koja implementira BibliotekaInterface.

---
+++ src/biblioteka/Biblioteka.java
@@ -0,0 +1,43 @@
+package biblioteka;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterface;
+
+public class Biblioteka implements BibliotekaInterface {
+
+	private List<Knjiga> knjige = new ArrayList<>();//implementacija liste preko nizova, prvih 16 popuni pa doda jos 16 itd
+	
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		List<Knjiga> rezultati = new ArrayList<Knjiga>();
+		
+		for (Knjiga k : knjige) {
+			if(k.getNaslov().toUpperCase().contains(naslov.toUpperCase())) {
+				rezultati.add(k);
+			}
+		}
+		
+		return rezultati;
+	}
+
+}
