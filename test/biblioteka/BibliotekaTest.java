9c4c9e32b1dae03ace8894e9dc26d81ea7cb226a
Mihajlo Ristanovic <mr20220302@student.fon.bg.ac.rs>
2026-05-06 15:17:14 +0200

    Ispravka importa.

--- test/biblioteka/BibliotekaTest.java
+++ test/biblioteka/BibliotekaTest.java
@@ -1,9 +1,5 @@
 package biblioteka;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
 import biblioteka.interfejs.BibliotekaInterface;
 import biblioteka.interfejs.BibliotekaInterfaceTest;
 
